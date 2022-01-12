5b21ac200a504836e19aa3b76be121388562617a
Michał Grzegorczyk <michalgrzegor@gmail.com>
2022-01-12 10:27:20 +0100

    fix no npmrc file (#12)

--- commands/release/index.js
+++ commands/release/index.js
@@ -26,13 +26,26 @@ class ReleaseCommand extends Command {
         },
       })
       .then(async (answer) => {
+        // check version and name
         const packageName = answer.package 
-        // export token part:
-        const npmrc = execSync(
-          `cat ~/.npmrc `,
+        const versionOutput = execSync(
+          `lerna list -a --scope=${packageName} --ndjson`,
           { encoding: 'utf-8' },
         );
-        const token = npmrc.slice(npmrc.indexOf('ghp_'), npmrc.indexOf('ghp_') + 40);
+        const packageInfo = JSON.parse(versionOutput)
+        const version = packageInfo.version
+        // export token part:
+        let npmrc;
+        let token;
+        try {
+          npmrc = execSync(
+            `cat ~/.npmrc`,
+            { encoding: 'utf-8' },
+          );
+        } catch {}
+        if(npmrc) {
+          token = npmrc.slice(npmrc.indexOf('ghp_'), npmrc.indexOf('ghp_') + 40);
+        }
         if(!token) {
           inquirer.prompt({
             type: 'input',
@@ -40,48 +53,45 @@ class ReleaseCommand extends Command {
             message: "Provide your github access token",
           }).then((tkn) => {
             process.env.GH_TOKEN = tkn.token;
+            this.bumpVersion(packageName, version);
           })
         }else{
           process.env.GH_TOKEN = token;
-        }
-
-        // check version
-        const version = execSync(
-          `lerna list -a --scope=${packageName} --ndjson`,
-          { encoding: 'utf-8' },
-        );
-        const packageInfo = JSON.parse(version)
-        if(packageInfo['version'].includes('alpha')) {
-          // prompt prerelease
-          inquirer.prompt({
-            type: 'list',
-            name: 'release',
-            message: "Do you want to graduate a package or bump to a release version?",
-            choices: ['pre-release', 'release'],
-          }).then((ver) => {
-            if(ver['release'] === 'pre-release') {
-              execSync(`lerna version  --force-publish=${packageName} --create-release github --ignore-changes '*'`, {stdio: 'inherit'});
-            } else {
-              execSync(`lerna version  --force-publish=${packageName} --conventional-graduate=${packageName} --create-release github --ignore-changes '*'`, {stdio: 'inherit'});
-            }
-          })
-        } else {
-          // prompt release
-          inquirer.prompt({
-            type: 'list',
-            name: 'release',
-            message: "Do you want to release next version or change to a prerelease version?",
-            choices: ['pre-release', 'release'],
-          }).then((ver) => {
-            if(ver['release'] === 'pre-release') {
-              console.log('dupa')
-              execSync(`lerna version  --force-publish=${packageName} --conventional-prerelease=${packageName} --create-release github --ignore-changes '*'`, {stdio: 'inherit'});
-            } else {
-              execSync(`lerna version  --force-publish=${packageName} --create-release github --ignore-changes '*'`, {stdio: 'inherit'});
-            }
-          })
+          this.bumpVersion(packageName, version);
         }
       });
+  }
+
+  bumpVersion(packageName, version) {
+    if(version.includes('alpha')) {
+      // prompt prerelease
+      inquirer.prompt({
+        type: 'list',
+        name: 'release',
+        message: "Do you want to graduate a package or bump to a release version?",
+        choices: ['pre-release', 'release'],
+      }).then((ver) => {
+        if(ver['release'] === 'pre-release') {
+          execSync(`lerna version  --force-publish=${packageName} --create-release github --ignore-changes '*'`, {stdio: 'inherit'});
+        } else {
+          execSync(`lerna version  --force-publish=${packageName} --conventional-graduate=${packageName} --create-release github --ignore-changes '*'`, {stdio: 'inherit'});
+        }
+      })
+    } else {
+      // prompt release
+      inquirer.prompt({
+        type: 'list',
+        name: 'release',
+        message: "Do you want to release next version or change to a prerelease version?",
+        choices: ['pre-release', 'release'],
+      }).then((ver) => {
+        if(ver['release'] === 'pre-release') {
+          execSync(`lerna version  --force-publish=${packageName} --conventional-prerelease=${packageName} --create-release github --ignore-changes '*'`, {stdio: 'inherit'});
+        } else {
+          execSync(`lerna version  --force-publish=${packageName} --create-release github --ignore-changes '*'`, {stdio: 'inherit'});
+        }
+      })
+    }
   }
 }
 
