5b9cd3ef07aa35fd56e5d4b522d0b0232618963d
Michał Grzegorczyk <michalgrzegor@gmail.com>
2022-04-02 15:01:24 +0200

    Michal/add regex and error log (#14)
    * fix no npmrc file
    
    * add logs for lerna error output | add regex github access token validation

--- commands/release/index.js
+++ commands/release/index.js
@@ -11,55 +11,60 @@ function factory(argv) {
 }
 
 class ReleaseCommand extends Command {
+  regex = /^ghp_[A-Za-z0-9]{36}$/
+
   async initialize() {}
 
   async execute() {
     return inquirer
-      .prompt({
-        type: 'list',
-        name: 'package',
-        message: "Select package to bump version",
-        choices: () => {
-          const output = execSync(`lerna list -a --json`, { encoding: 'utf-8' });
-          const packages = JSON.parse(output)
-          return packages.map((pckg) => pckg.name)
-        },
-      })
-      .then(async (answer) => {
-        // check version and name
-        const packageName = answer.package 
-        const versionOutput = execSync(
-          `lerna list -a --scope=${packageName} --ndjson`,
+    .prompt({
+      type: 'list',
+      name: 'package',
+      message: "Select package to bump version",
+      choices: () => {
+        const output = execSync(`lerna list -a --json`, { encoding: 'utf-8' });
+        const packages = JSON.parse(output)
+        return packages.map((pckg) => pckg.name)
+      },
+    })
+    .then(async (answer) => {
+      // check version and name
+      const packageName = answer.package 
+      const versionOutput = execSync(
+        `lerna list -a --scope=${packageName} --ndjson`,
+        { encoding: 'utf-8' },
+      );
+      const packageInfo = JSON.parse(versionOutput)
+      const version = packageInfo.version
+      // export token part:
+      let token;
+      try {
+        const npmrc = execSync(
+          `cat ~/.npmrc`,
           { encoding: 'utf-8' },
-        );
-        const packageInfo = JSON.parse(versionOutput)
-        const version = packageInfo.version
-        // export token part:
-        let npmrc;
-        let token;
-        try {
-          npmrc = execSync(
-            `cat ~/.npmrc`,
-            { encoding: 'utf-8' },
           );
-        } catch {}
-        if(npmrc) {
-          token = npmrc.slice(npmrc.indexOf('ghp_'), npmrc.indexOf('ghp_') + 40);
+        token = npmrc.slice(npmrc.indexOf('ghp_'), npmrc.indexOf('ghp_') + 40);
+        if(!token || !this.regex.test(token)) {
+          throw error();
         }
-        if(!token) {
-          inquirer.prompt({
-            type: 'input',
-            name: 'token',
-            message: "Provide your github access token",
-          }).then((tkn) => {
-            process.env.GH_TOKEN = tkn.token;
-            this.bumpVersion(packageName, version);
-          })
-        }else{
-          process.env.GH_TOKEN = token;
+        process.env.GH_TOKEN = token;
+        this.bumpVersion(packageName, version);
+      } catch {
+        const noToken = 'you do not have a .npmrc file or it does not contain a token';
+        const wrongFormat = 'token format is not valid'
+        const message = token.length === 0 ?  noToken : wrongFormat;
+        console.log('\x1b[42m%s\x1b[0m', message)
+        inquirer.prompt({
+          type: 'input',
+          name: 'token',
+          message: "Provide your github access token",
+          validate: (input) => this.regex.test(input) ? true : 'provided token is invalid'
+        }).then((tkn) => {
+          process.env.GH_TOKEN = tkn.token;
           this.bumpVersion(packageName, version);
-        }
-      });
+        })
+      }
+    });
   }
 
   bumpVersion(packageName, version) {
@@ -76,7 +81,7 @@ class ReleaseCommand extends Command {
         } else {
           execSync(`lerna version  --force-publish=${packageName} --conventional-graduate=${packageName} --create-release github --ignore-changes '*'`, {stdio: 'inherit'});
         }
-      })
+      }).catch((error) => this.logError(error))
     } else {
       // prompt release
       inquirer.prompt({
@@ -90,8 +95,14 @@ class ReleaseCommand extends Command {
         } else {
           execSync(`lerna version  --force-publish=${packageName} --create-release github --ignore-changes '*'`, {stdio: 'inherit'});
         }
-      })
+      }).catch((error) => this.logError(error))
     }
+  }
+
+  logError(error) {
+    console.log('\x1b[45m%s\x1b[0m', 'lerna error:')
+    console.log(error)
+    console.log("sdterr",error.stderr.toString())
   }
 }
 
